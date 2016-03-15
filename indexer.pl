7dd970e1d98f6ddc2cb545531569cfa3e5c73def
Chocobozzz <florian.bigard@gmail.com>
2016-03-15 16:42:10 +0100

    Progress

---
+++ indexer.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+my $radio_name = shift @ARGV or die "Need the radio name as argument";
+my $id = 1;
+
+while (<>) {
+  my $time = time * 1000;
+  `curl -XPUT http://localhost:9200/radios/radios/$id -d '{ "date": $time, "name": "$radio_name", "data": "$_" }'`;
+  $id++;
+}
