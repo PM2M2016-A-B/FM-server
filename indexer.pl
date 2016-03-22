2686ad92b5f53a8b79b98b52429e1e3e8ad92a44
Chocobozzz <florian.bigard@gmail.com>
2016-03-22 14:43:50 +0100

    coucou

--- indexer.pl
+++ indexer.pl
@@ -6,7 +6,9 @@ my $radio_name = shift @ARGV or die "Need the radio name as argument";
 my $id = 1;
 
 while (<>) {
+  chomp;
   my $time = time * 1000;
-  `curl -XPUT http://localhost:9200/radios/radios/$id -d '{ "date": $time, "name": "$radio_name", "data": "$_", "data_raw": "$_" }'`;
+  my $command = "curl -s -XPUT http://ec2-54-209-134-58.compute-1.amazonaws.com:9200/radios/radios/$id -d '{ \"date\": $time, \"name\": \"$radio_name\", \"data\": \"$_\" }'";
+ `$command`;
   $id++;
 }
