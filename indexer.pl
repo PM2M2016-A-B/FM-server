c79f1598c5643958c06d4e0c62e99f18ec0e1bbd
Chocobozzz <florian.bigard@gmail.com>
2016-03-29 14:02:23 +0200

    Try to use the elastic search module

--- indexer.pl
+++ indexer.pl
@@ -1,6 +1,11 @@
 #!/usr/bin/perl -w
 
 use strict;
+use Search::Elasticsearch;
+
+my $e = Search::Elasticsearch->new(
+    nodes    => 'ec2-54-209-134-58.compute-1.amazonaws.com:9200'
+);
 
 my $radio_name = shift @ARGV or die "Need the radio name as argument";
 my $id = 1;
@@ -10,6 +15,17 @@ while (<>) {
   s/ +/ /g;
   my $time = time * 1000;
   my $command = "curl -s -XPUT http://ec2-54-209-134-58.compute-1.amazonaws.com:9200/radios/radios/$id -d '{ \"date\": $time, \"name\": \"$radio_name\", \"data\": \"$_\", \"data_raw\": \"$_\" }'";
- `$command`;
+  $e->index(
+    index   => 'radios',
+    type    => 'radios',
+    id      => $id,
+    body    => {
+      date => $time,
+      name => $radio_name,
+      data => $_,
+      data_raw => $_
+    }
+  );
+
   $id++;
 }
