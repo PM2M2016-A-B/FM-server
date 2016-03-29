2b303c38403d62c6650e50eb98e54eacce10dfec
Ubuntu <ubuntu@ip-172-31-61-59.ec2.internal>
2016-03-29 11:56:51 +0000

    Rmove space

--- indexer.pl
+++ indexer.pl
@@ -7,6 +7,7 @@ my $id = 1;
 
 while (<>) {
   chomp;
+  s/ +/ /g;
   my $time = time * 1000;
   my $command = "curl -s -XPUT http://ec2-54-209-134-58.compute-1.amazonaws.com:9200/radios/radios/$id -d '{ \"date\": $time, \"name\": \"$radio_name\", \"data\": \"$_\", \"data_raw\": \"$_\" }'";
  `$command`;
